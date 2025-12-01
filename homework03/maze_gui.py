6a3e0d0f330bcb9c6d6b5414bcdfb268fc1cbf31
Софья Баранова <sofiabaranova@MacBook-Pro-Sofa-2.local>
2025-12-01 17:44:23 +0300

    maze_gui fixed

--- homework03/maze_gui.py
+++ homework03/maze_gui.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-from typing import List
-from tkinter import ttk, messagebox
-from maze import bin_tree_maze, solve_maze, add_path_to_grid
 from copy import deepcopy
+from tkinter import messagebox, ttk
+from typing import List
+
+from maze import add_path_to_grid, bin_tree_maze, solve_maze
 
 
 def draw_cell(x, y, color, size: int = 10):
