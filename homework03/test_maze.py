fb8db8c24a22b0a7788fcbcf821614fce5328395
Софья Баранова <sofiabaranova@MacBook-Pro-Sofa-2.local>
2025-12-01 17:58:02 +0300

    Format maze_gui.py with black

--- homework03/test_maze.py
+++ homework03/test_maze.py
@@ -274,9 +274,7 @@ class MazeTest(unittest.TestCase):
             ["■", "■", "■", 5, "■"],
         ]
         second_exit_1 = (4, 3)
-        self.assertEqual(
-            [(4, 3), (3, 3), (3, 2), (3, 1), (3, 0)], maze.shortest_path(grid_1, second_exit_1)
-        )
+        self.assertEqual([(4, 3), (3, 3), (3, 2), (3, 1), (3, 0)], maze.shortest_path(grid_1, second_exit_1))
 
         grid_2 = [
             ["■", "■", "■", "■", "■", 1, "■"],
@@ -328,5 +326,3 @@ class MazeTest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-
